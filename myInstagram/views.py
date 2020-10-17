3a80ae16d5679596626f813df6940fa2e8f8a766
dancan-sandys <dancan.oruko96@gmail.com>
2020-10-17 12:50:44 +0300

    add the photos , profiles and single image views

--- myInstagram/views.py
+++ myInstagram/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, request
+from models import Photo,Profile,Comments
 
 # Create your views here.
+
+#user profile
+def profile(request, id):
+    user = profile.Profile.objects.get(id = id)
+
+    return render(request, 'profile.html', {"user":user})
+
+def photos(request):
+
+    Photos = Photo.objects.all()
+
+    return render(request, 'photos.html', {"photos":photos})
+
+def image(request, id):
+
+    image = Photo.objects.get(id =id)
+
+    return render(request, 'image.html', {"photo": image})
